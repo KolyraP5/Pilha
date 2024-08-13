00b3da4a63d666429ccc9624a43a3cb9bae39b64
KolyraP5 <pecoly2005@gmail.com>
2024-08-13 16:37:09 -0300

    Update Pilha.java

--- Pilha.java
+++ Pilha.java
@@ -2,5 +2,27 @@ public class Pilha {
     private int capacidade;
     private int topo;
     private int [] dados;
-}
 
+    public Pilha (int capacidade) {
+        dados = new int[capacidade];
+        topo = -1;
+    }
+
+    public int inserir(int dados){
+        topo ++;
+        dados[topo] = dados;
+        return dados;
+
+    }
+
+    private int cheia(int capacidade){
+        if (topo == capacidade){
+            System.out.print("a pilha está cheia");
+        }
+        return capacidade;
+    }
+    public void main(String[] args) {
+        inserir(5);
+        cheia(10);
+    }
+}
